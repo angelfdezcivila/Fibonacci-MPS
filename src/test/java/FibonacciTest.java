6e2f78304ae35e405db0f6100df310888bcbba94
angelfdezcivila <angelfdezcivila@uma.es>
2022-03-07 18:38:41 +0100

    Tests a medias y Fibonacci.java planteado

--- src/test/java/FibonacciTest.java
+++ src/test/java/FibonacciTest.java
@@ -1,5 +1,31 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FibonacciTest {
+    private Fibonacci fibonacci;
 
+    @Test
+    public void computeOfZeroReturnZero() {
+        int obtainedValue = fibonacci.compute(0);
+        int expectedValue = 0;
+        assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    public void computeOfOneReturnOne() {
+        int obtainedValue = fibonacci.compute(0);
+        int expectedValue = 0;
+        assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    public void computeOfANegativeNumberThrowAnException() {
+        assertThrows(RuntimeException.class, () -> fibonacci.compute(-1));
+    }
+
+    @Test
+    public void ComputeOfANumberBiggerThan45ThrowAnException() {
+        assertThrows(RuntimeException.class, () -> fibonacci.compute(46));
+    }
 }
